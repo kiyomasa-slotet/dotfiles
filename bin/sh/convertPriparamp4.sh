cc734eea7796d43d50a543a26b2396c02139dfdf
pullingour <kiyomasa.sato.github@gmail.com>
2017-11-17 01:58:55 +0900

    hoge

--- bin/sh/convertPriparamp4.sh
+++ bin/sh/convertPriparamp4.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-cd $HOME/Desktop;
-read -p "select gif setting: " num
-case $num in
-  1) ffmpeg -ss 00:00:00 -i *.mp4 -vf "transpose=2" output.mp4
-    ;;
-  2) ffmpeg -ss 00:00:00 -i Untitled.mov -vf fps=24,scale=320:180 cut.mov
-    ;;
-  3) ffmpeg -ss 00:00:00 -i Untitled.mov -vf fps=9,scale=506:278 cut.mov
-    ;;
-esac
+cd $HOME/Desktop
+ffmpeg -ss 00:00:00 -i Untitled.MOV -vf fps=24,scale=540:960 output.mp4
 echo "done"
 open output.mp4
