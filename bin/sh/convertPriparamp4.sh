2297b589972f28b781eff5ea8cbe7bae3d6681e1
pullingour <kiyomasa.sato.github@gmail.com>
2017-11-18 01:26:32 +0900

    hoge

--- bin/sh/convertPriparamp4.sh
+++ bin/sh/convertPriparamp4.sh
@@ -1,6 +1,34 @@
 #!/bin/bash
 
+timestamp=$(date +%s)
+echo $timestamp
+
 cd $HOME/Desktop
-ffmpeg -ss 00:00:00 -i Untitled.MOV -vf fps=24,scale=540:960 output.mp4
+ffmpeg -i input.mov -c:v libvpx -crf 10 -b:v 1M -c:a libvorbis -vf fps=30,scale=630:1120 $timestamp.webm
+
 echo "done"
-open output.mp4
+open $timestamp.webm
+
+
+<< COMMENTOUT
+
+SIZE:
+ - 16:9
+ - 540:960
+ - 1120:630
+   and more...
+
+SOUND:
+ - acodec libopus -b:a 80k
+ - acodec libopus -b:a 128k
+   to mono
+
+TODO:
+ - make capture(random time?)
+ - 何より先にindexページレイアウト作成だ、シンプルなもので構わない
+
+
+
+
+
+COMMENTOUT
